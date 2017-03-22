1ce094090bd3f062e1d59a191c717a00852a88fe
Sungsoo Moon <newms@Sungsoos-iMac.local>
2017-03-22 17:56:07 +0900

    Fix indents

--- skiplist_test.c
+++ skiplist_test.c
@@ -32,62 +32,62 @@ main(void)
     int i;
     struct timespec start, end;
 
-    int *key = malloc(N * sizeof(int));
-    if (key == NULL) {
-        exit(-1);
-    }
-
-    struct skiplist *list = skiplist_new();
-    if (list == NULL) {
-        exit(-1);
-    }
-
-    printf("Test start!\n");
-    printf("Add %d nodes...\n", N);
-
-    /* Insert test */
-    srandom(time(NULL));
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (i = 0; i < N; i++) {
-        int value = key[i] = (int)random();
-        skiplist_insert(list, key[i], value);
-    }
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-    #ifdef SKIPLIST_DEBUG
-    skiplist_dump(list);
-    #endif
-
-    /* Search test */
-    printf("Now search each node...\n");
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (i = 0; i < N; i++) {
-        struct skipnode *node = skiplist_search(list, key[i]);
-        if (node != NULL) {
-            #ifdef SKIPLIST_DEBUG
-            printf("key:0x%08x value:0x%08x\n", node->key, node->value);
-            #endif
-        } else {
-            printf("Not found:0x%08x\n", key[i]);
-        }
-    }
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-
-    /* Delete test */
-    printf("Now remove all nodes...\n");
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (i = 0; i < N; i++) {
-        skiplist_remove(list, key[i]);
-    }
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-    #ifdef SKIPLIST_DEBUG
-    skiplist_dump(list);
-    #endif
-
-    printf("End of Test.\n");
-    skiplist_delete(list);
+    // int *key = malloc(N * sizeof(int));
+    // if (key == NULL) {
+    //     exit(-1);
+    // }
+    //
+    // struct skiplist *list = skiplist_new();
+    // if (list == NULL) {
+    //     exit(-1);
+    // }
+    //
+    // printf("Test start!\n");
+    // printf("Add %d nodes...\n", N);
+    //
+    // /* Insert test */
+    // srandom(time(NULL));
+    // clock_gettime(CLOCK_MONOTONIC, &start);
+    // for (i = 0; i < N; i++) {
+    //     int value = key[i] = (int)random();
+    //     skiplist_insert(list, key[i], value);
+    // }
+    // clock_gettime(CLOCK_MONOTONIC, &end);
+    // printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+    // #ifdef SKIPLIST_DEBUG
+    // skiplist_dump(list);
+    // #endif
+    //
+    // /* Search test */
+    // printf("Now search each node...\n");
+    // clock_gettime(CLOCK_MONOTONIC, &start);
+    // for (i = 0; i < N; i++) {
+    //     struct skipnode *node = skiplist_search(list, key[i]);
+    //     if (node != NULL) {
+    //         #ifdef SKIPLIST_DEBUG
+    //         printf("key:0x%08x value:0x%08x\n", node->key, node->value);
+    //         #endif
+    //     } else {
+    //         printf("Not found:0x%08x\n", key[i]);
+    //     }
+    // }
+    // clock_gettime(CLOCK_MONOTONIC, &end);
+    // printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+    //
+    // /* Delete test */
+    // printf("Now remove all nodes...\n");
+    // clock_gettime(CLOCK_MONOTONIC, &start);
+    // for (i = 0; i < N; i++) {
+    //     skiplist_remove(list, key[i]);
+    // }
+    // clock_gettime(CLOCK_MONOTONIC, &end);
+    // printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+    // #ifdef SKIPLIST_DEBUG
+    // skiplist_dump(list);
+    // #endif
+    //
+    // printf("End of Test.\n");
+    // skiplist_delete(list);
 
     return 0;
 }

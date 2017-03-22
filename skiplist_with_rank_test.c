d50e4289bb91f8829316e32c2caacb146d280859
Sungsoo Moon <newms@Sungsoos-iMac.local>
2017-03-22 17:53:05 +0900

    Fix indents

--- skiplist_with_rank_test.c
+++ skiplist_with_rank_test.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015, Leo Ma <begeekmyfriend@gmail.com>
- */
+* Copyright (C) 2015, Leo Ma <begeekmyfriend@gmail.com>
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,86 +30,86 @@ int clock_gettime(int clk_id, struct timespec* t) {
 int
 main(void)
 {
-        int i;
-        struct timespec start, end;
+    int i;
+    struct timespec start, end;
 
-        int *key = (int *)malloc(N * sizeof(int));
-        if (key == NULL) {
-                exit(-1);
+    int *key = (int *)malloc(N * sizeof(int));
+    if (key == NULL) {
+        exit(-1);
+    }
+
+    struct skiplist *list = skiplist_new();
+    if (list == NULL) {
+        exit(-1);
+    }
+
+    printf("Test start!\n");
+    printf("Add %d nodes...\n", N);
+
+    /* Insert test */
+    srandom(time(NULL));
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (i = 0; i < N; i++) {
+        int value = key[i] = (int)random();
+        skiplist_insert(list, key[i], value);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+    #ifdef SKIPLIST_DEBUG
+    skiplist_dump(list);
+    #endif
+
+    /* Search test 1 */
+    printf("Now search each node by key...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (i = 0; i < N; i++) {
+        struct skipnode *node = skiplist_search_by_key(list, key[i]);
+        if (node != NULL) {
+            #ifdef SKIPLIST_DEBUG
+            printf("key:0x%08x value:0x%08x\n", node->key, node->value);
+            #endif
+        } else {
+            printf("Not found:0x%08x\n", key[i]);
         }
+        #ifdef SKIPLIST_DEBUG
+        printf("key rank:%d\n", skiplist_key_rank(list, key[i]));
+        #else
+        //skiplist_key_rank(list, key[i]);
+        #endif
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
 
-        struct skiplist *list = skiplist_new();
-        if (list == NULL) {
-                exit(-1);
+    /* Search test 2 */
+    printf("Now search each node by rank...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (i = 0; i < N; i++) {
+        struct skipnode *node = skiplist_search_by_rank(list, i + 1);
+        if (node != NULL) {
+            #ifdef SKIPLIST_DEBUG
+            printf("rank:%d value:0x%08x\n", i + 1, node->value);
+            #endif
+        } else {
+            printf("Not found:%d\n", i + 1);
         }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
 
-        printf("Test start!\n");
-        printf("Add %d nodes...\n", N);
+    /* Delete test */
+    printf("Now remove all nodes...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (i = 0; i < N; i++) {
+        skiplist_remove(list, key[i]);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
+    #ifdef SKIPLIST_DEBUG
+    skiplist_dump(list);
+    #endif
 
-        /* Insert test */
-        srandom(time(NULL));
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        for (i = 0; i < N; i++) {
-                int value = key[i] = (int)random();
-                skiplist_insert(list, key[i], value);
-        }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-#ifdef SKIPLIST_DEBUG
-        skiplist_dump(list);
-#endif
+    printf("End of Test.\n");
+    skiplist_delete(list);
 
-        /* Search test 1 */
-        printf("Now search each node by key...\n");
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        for (i = 0; i < N; i++) {
-                struct skipnode *node = skiplist_search_by_key(list, key[i]);
-                if (node != NULL) {
-#ifdef SKIPLIST_DEBUG
-                        printf("key:0x%08x value:0x%08x\n", node->key, node->value);
-#endif
-                } else {
-                        printf("Not found:0x%08x\n", key[i]);
-                }
-#ifdef SKIPLIST_DEBUG
-                printf("key rank:%d\n", skiplist_key_rank(list, key[i]));
-#else
-                //skiplist_key_rank(list, key[i]);
-#endif
-        }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-
-        /* Search test 2 */
-        printf("Now search each node by rank...\n");
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        for (i = 0; i < N; i++) {
-                struct skipnode *node = skiplist_search_by_rank(list, i + 1);
-                if (node != NULL) {
-#ifdef SKIPLIST_DEBUG
-                        printf("rank:%d value:0x%08x\n", i + 1, node->value);
-#endif
-                } else {
-                        printf("Not found:%d\n", i + 1);
-                }
-        }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-
-        /* Delete test */
-        printf("Now remove all nodes...\n");
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        for (i = 0; i < N; i++) {
-                skiplist_remove(list, key[i]);
-        }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        printf("time span: %ldms\n", (end.tv_sec - start.tv_sec)*1000 + (end.tv_nsec - start.tv_nsec)/1000000);
-#ifdef SKIPLIST_DEBUG
-        skiplist_dump(list);
-#endif
-
-        printf("End of Test.\n");
-        skiplist_delete(list);
-
-        return 0;
+    return 0;
 }

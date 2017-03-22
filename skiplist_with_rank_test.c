0d845258c39e87c50ab47244d045962609d75067
Sungsoo Moon <newms@Sungsoos-iMac.local>
2017-03-22 17:45:52 +0900

    Fix compile problem on mac osx

--- skiplist_with_rank_test.c
+++ skiplist_with_rank_test.c
@@ -4,7 +4,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#if defined(__MACH__) && !defined(CLOCK_REALTIME)
+#include <sys/time.h>
+
+#define CLOCK_REALTIME 0
+#define CLOCK_MONOTONIC 1
+
+int clock_gettime(int clk_id, struct timespec* t) {
+    struct timeval now;
+    int rv = gettimeofday(&now, NULL);
+    if (rv) return rv;
+    t->tv_sec  = now.tv_sec;
+    t->tv_nsec = now.tv_usec * 1000;
+    return 0;
+}
+#else
 #include <time.h>
+#endif
 
 #include "skiplist_with_rank.h"
 
